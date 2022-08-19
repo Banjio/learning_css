4f2f908aa8e3fe7ddfce94b387c4f46e57c2fca6
Maximilian Beier <beiersmaxi@gmail.com>
2022-08-19 23:01:23 +0200

    Wlkhtmltopdf script

--- htmltopdf.sh
+++ htmltopdf.sh
@@ -1,1 +1,3 @@
-wkhtmltopdf --enable-local-file-access online_cv/content/pages/cv.html out.pdf
+#wkhtmltopdf --enable-local-file-access online_cv/content/pages/cv.html out.pdf
+
+wkhtmltopdf --orientation Landscape -enable-local-file-access online_cv/content/pages/cv.html out.pdf
